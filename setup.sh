79a0df0bfcd93d50750584e89615900ba1d86965
Ryan Cheley <rcheley@gmail.com>
2023-06-04 14:23:42 -0700

    Add Git configuration

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Enable shell script strictness
+set -eu
+
+# Enable command tracing
+set -x
+
+# Ensure config directory exists
+mkdir -p ~/.config
+
+# Link Git config if it doesn't exist
+[ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
