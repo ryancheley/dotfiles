23a5da069c17b399ba5bc8bf51bbeaba226ff7ca
Ryan Cheley <rcheley@gmail.com>
2023-06-20 20:01:00 -0700

    Added starship configuration

--- setup.sh
+++ setup.sh
@@ -11,3 +11,6 @@ mkdir -p ~/.config
 
 # Link Git config if it doesn't exist
 [ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
+
+# Link Starship config if it doesn't exist
+[ ! -e ~/.config/starship.toml ] && ln -s "$PWD/config/starship/starship.toml" ~/.config/starship.toml
