199cff9298d0671f64c1bf14f9173895eb9d6a6a
Ryan Cheley <rcheley@gmail.com>
2025-08-15 16:05:12 -0700

    :wrench: add fish

---
+++ config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    atuin init fish | source
+    abbr --add gcmsg git commit -m
+    abbr --add ga git add
+    abbr --add act source .venv/bin/activate.fish
+    abbr --add deact deactivate
+    abbr --add g git
+    abbr --add gsw git switch
+    abbr --add gl git pull
+    abbr --add gp git push
+    abbr --add la ls -lAh
+    abbr --add l ls -lah
+    abbr --add gst git status
+    abbr --add lsa ls -lah
+end
+
+starship init fish | source
--- setup.sh
+++ setup.sh
@@ -12,6 +12,9 @@ mkdir -p ~/.config
 # Link Git config if it doesn't exist
 [ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
 
+# Link fish config if it doesn't exist
+[ ! -e ~/.config/fish ] && ln -s "$PWD/config/fish" ~/.config/fish
+
 # Link Starship config if it doesn't exist
 [ ! -e ~/.config/starship.toml ] && ln -s "$PWD/config/starship/starship.toml" ~/.config/starship.toml
 
